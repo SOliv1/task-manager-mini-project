a52495020c818285d3cc40993888c565be974978
Sara Oliver <s.oliver1@icloud.com>
2020-02-18 22:33:42 +0000

    initial commit

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId 
 from os import path
+
+
+
 if path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 app.config["MONGO_DBNAME"] = 'task_manager'
-app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 
 mongo = PyMongo(app)
 
