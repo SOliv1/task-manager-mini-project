31afd821e104cc751b5832b3d8f7b5b4aba028f4
Sara Oliver <s.oliver1@icloud.com>
2020-02-18 15:18:11 +0000

    add templates-tasks

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
+from os import path
+if path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'task_manager'
+app.config["MONGO_URI"] = os.getenv('MONGO_URI')
+
+mongo = PyMongo(app)
 
 
 @app.route('/')
-def hello():
-    return 'Hello World ...again'
+@app.route('/get_tasks')
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
 if __name__ == '__main__':
