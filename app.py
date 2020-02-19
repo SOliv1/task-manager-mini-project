65110c877f6eeaab66a8b2e6212d1612b84af35d
Sara Oliver <s.oliver1@icloud.com>
2020-02-19 12:02:08 +0000

    added a test print

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
+    print(mongo.db.tasks.find().count())
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
