9edfcf5e3840566a47e9486d5b5b05bc1bac4e47
Sara Oliver <s.oliver1@icloud.com>
2020-02-20 13:53:13 +0000

    connect to mdatlasdb

--- app.py
+++ app.py
@@ -24,12 +24,15 @@ def get_tasks():
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-                           categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())                           
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for('get_tasks'))    
 
     
-
-
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
