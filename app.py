cffb21e1c1a1dc1cac40972b806c11cd2602353f
Sara Oliver <s.oliver1@icloud.com>
2020-02-20 23:07:05 +0000

    delete tasks

--- app.py
+++ app.py
@@ -18,25 +18,27 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+    return render_template("tasks.html", 
+                           tasks=mongo.db.tasks.find())
 
 
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-                           categories=mongo.db.categories.find())                           
+                           categories=mongo.db.categories.find())
+
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks = mongo.db.tasks
+    tasks =  mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
-    return redirect(url_for('get_tasks'))  
+    return redirect(url_for('get_tasks'))
 
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories = mongo.db.categories.find()
+    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories =  mongo.db.categories.find()
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
 
@@ -45,15 +47,20 @@ def edit_task(task_id):
 def update_task(task_id):
     tasks = mongo.db.tasks
     tasks.update( {'_id': ObjectId(task_id)},
-
     {
         'task_name':request.form.get('task_name'),
         'category_name':request.form.get('category_name'),
         'task_description': request.form.get('task_description'),
         'due_date': request.form.get('due_date'),
-        'is_urgent': request.form.get('is_urgent')
+        'is_urgent':request.form.get('is_urgent')
     })
-    return redirect(url_for('get_tasks'))  
+    return redirect(url_for('get_tasks'))
+
+
+@app.route('/delete_task/<task_id>')
+def delete_task(task_id):
+    mongo.db.tasks.remove({'_id': ObjectId(task_id)})
+    return redirect(url_for('get_tasks'))
 
 
 if __name__ == '__main__':
