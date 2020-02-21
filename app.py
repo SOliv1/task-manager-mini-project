17aab05993f80af9b17a01dcec15f39d79583174
Sara Oliver <s.oliver1@icloud.com>
2020-02-21 11:46:12 +0000

    delete category and redirect

--- app.py
+++ app.py
@@ -84,6 +84,12 @@ def update_category(category_id):
     return redirect(url_for('get_categories'))
 
 
+@app.route('/delete_category/<category_id>')
+def delete_category(category_id):
+    mongo.db.categories.remove({'_id': ObjectId(category_id)})
+    return redirect(url_for('get_categories'))
+
+
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
