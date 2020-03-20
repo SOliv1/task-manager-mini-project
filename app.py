d60ec05010e6634fc6405e0d3ac707d303efb113
Sara Oliver <s.oliver1@icloud.com>
2020-03-20 13:51:14 +0000

    housekeeping

--- app.py
+++ app.py
@@ -25,20 +25,20 @@ def get_tasks():
 @app.route('/add_task')
 def add_task():
     return render_template('addtask.html',
-        categories=mongo.db.categories.find())
+                           categories=mongo.db.categories.find())
 
 
 @app.route('/insert_task', methods=['POST'])
 def insert_task():
-    tasks =  mongo.db.tasks
+    tasks = mongo.db.tasks
     tasks.insert_one(request.form.to_dict())
     return redirect(url_for('get_tasks'))
 
 
 @app.route('/edit_task/<task_id>')
 def edit_task(task_id):
-    the_task =  mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
-    all_categories =  mongo.db.categories.find()
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
     return render_template('edittask.html', task=the_task,
                            categories=all_categories)
 
@@ -68,6 +68,7 @@ def get_categories():
     return render_template('categories.html',
                            categories=mongo.db.categories.find())
 
+
 @app.route('/delete_category/<category_id>')
 def delete_category(category_id):
     mongo.db.categories.remove({'_id': ObjectId(category_id)})
@@ -78,7 +79,7 @@ def delete_category(category_id):
 def edit_category(category_id):
     return render_template('editcategory.html',
                            category=mongo.db.categories.find_one(
-                           {'_id': ObjectId(category_id)}))
+                                    {'_id': ObjectId(category_id)}))
 
 
 @app.route('/update_category/<category_id>', methods=['POST'])
@@ -101,8 +102,7 @@ def add_category():
     return render_template('addcategory.html')
 
 
-
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+            debug=True)        
---
+++ env.py
@@ -0,0 +1,6 @@
+
+import os
+
+os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstmdb-zpki6.mongodb.net/task_manager?retryWrites=true&w=majority"
+
+os.environ["MONGO_DBNAME"] = "task_manager"
