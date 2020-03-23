788f705412f40e494e253147ee6b05c8e5a4f03a
Sara Oliver <s.oliver1@icloud.com>
2020-03-23 19:48:10 +0000

    found a couple of bugs and updated

--- env.py
+++ env.py
@@ -2,5 +2,4 @@
 import os
 
 os.environ["MONGO_URI"] = "mongodb+srv://root:r00tUser@myfirstmdb-zpki6.mongodb.net/task_manager?retryWrites=true&w=majority"
-
 os.environ["MONGO_DBNAME"] = "task_manager"
